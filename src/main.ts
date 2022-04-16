9e85d084fa4c6ff114b2dedcfc7fa61c3cef097d
TheNinza <niks.a3198@gmail.com>
2022-04-16 17:44:19 +0530

    modified for 4x4 grid

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import data from "./MOCK_DATA.json";
 
 const app = document.getElementById("app")!;
 
-const transformedData = data.map((item) => ({
-  ...item,
-  id: item.id.toString(),
-}));
+const transformedData = data
+  .map((item) => ({
+    ...item,
+    id: item.id.toString(),
+  }))
+  .slice(0, 16);
 
 const handleClick = (imgSlug: string) => {
   console.log(imgSlug);
